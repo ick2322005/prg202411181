507337c12d234cc300ea4bff5c047cb0bdbd4921
ick2322005 <ick2322005@ichiko.ed.jp>
2025-01-14 14:48:38 +0900

    たぶんいける

---
+++ style.cs
@@ -0,0 +1,21 @@
+background{
+    color: rgb(0,0,0)
+}
+body {
+    font-weight: bolder;
+    font-family: system-ui;
+}
+
+h1 {
+    color: rgba(255, 0, 0, 0.952);
+    background-color: rgb(0, 0, 0);
+}
+
+h2 {
+    color: rgb(251, 255, 0);
+    background-color: black;
+}
+
+h6{
+    color: rgb(255, 0, 0);
+}
